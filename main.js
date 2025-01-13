6c747454730d44f41a3da365be1c06ab06fb75ef
Olusola Ogunsola <Sundayshola48@gmail.com>
2025-01-12 23:21:08 -1100

    first

--- main.js
+++ main.js
@@ -1,1 +1,62 @@
- if
+let user = "Olusola"
+let password = 1234
+
+if (user == "Olusola" && password == 1234){console.log("Access granted")}
+else {
+    console.log("Access Denied")
+}
+
+
+
+//SWITCH STATEMENT
+
+switch(key)  {
+
+    case value:
+
+        break;
+
+    default:
+
+
+}
+
+//e.g on switch
+
+let dayOfTheWeek = 1;
+
+switch(dayOfTheWeek) {
+
+    case 1:
+
+    console.log("Sunday")
+        break;
+
+    case 2:
+
+    console.log("Monday")
+
+}
+
+let score = 100;
+
+switch (score){
+
+    case 100:
+        console.log("Excellent")
+        break;
+
+
+    case 90:
+        console.log("Perfect")
+        break;
+
+
+    case 80:
+        console.log("Good")
+        break;
+
+}
+
+// ternary operator
+
